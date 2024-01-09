45305b12727161afd176fcc5ba008ba84484350e
Kevin Schwarz <kschwarz99@gmail.com>
2024-01-09 15:10:39 -0500

    updated main char sprite and movement animations

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections;
+
 
 public class PlayerController : MonoBehaviour
 {
@@ -7,6 +9,12 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 5f;
     private Rigidbody2D rb;
     private Vector2 movement;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+    private bool isRolling = false;
+    private float originalSpeed = 5f;
+    public float rollSpeedMultiplier = 1.5f;
+    public float rollDuration = 0.34f;
 
     void Awake()
     {
@@ -22,13 +30,53 @@ public class PlayerController : MonoBehaviour
         }
 
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        // Flip the character's sprite based on movement direction
+        if (movement.x > 0)
+        {
+            spriteRenderer.flipX = false; // Face right
+        }
+        else if (movement.x < 0)
+        {
+            spriteRenderer.flipX = true; // Face left
+        }
+
+        // Set the Speed parameter to simulate movement for debugging
+        float simulatedSpeed = movement.sqrMagnitude > 0 ? 0.5f : 0f;
+        animator.SetFloat("Speed", simulatedSpeed);
+
+        if (Input.GetKeyDown(KeyCode.Space) && !isRolling)
+        {
+            Debug.Log("Spacebar pressed and not rolling.");
+            StartCoroutine(Roll());
+        }
+
     }
+
+
+    private IEnumerator Roll()
+    {
+        Debug.Log("Coroutine started.");
+        isRolling = true;
+        animator.SetTrigger("RollNow");
+        Debug.Log("RollNow trigger set.");
+        moveSpeed *= rollSpeedMultiplier;
+        yield return new WaitForSeconds(rollDuration);
+
+        moveSpeed = originalSpeed;
+        isRolling = false;
+        Debug.Log("Coroutine ended.");
+    }
+
+
 
     void FixedUpdate()
     {
